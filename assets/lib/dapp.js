ea5824bd27e5eb2b7ebb5840e4ea788fcaffa972
Arcadium Network <arcadiumnetworkcore@gmail.com>
2020-05-16 00:41:57 +1000

    Upgrades
    Stake, don't freeze

--- assets/lib/dapp.js
+++ assets/lib/dapp.js
@@ -1,83 +1,17 @@
 async function run() {
-    var abi = [
-        {
-            constant: true,
-            inputs: [{internalType: 'address',name: '_user',type: 'address'}],
-            name: 'allInfoFor',
-            outputs: [
-                {internalType: 'uint256',name: 'totalTokenSupply',type: 'uint256'},
-                {internalType: 'uint256',name: 'totalTokensFrozen',type: 'uint256'},
-                {internalType: 'uint256',name: 'userBalance',type: 'uint256'},
-                {internalType: 'uint256',name: 'userFrozen',type: 'uint256'},
-                {internalType: 'uint256',name: 'userDividends',type: 'uint256'}
-            ],
-            payable: false,stateMutability: 'view',type: 'function'
-        },
-        {
-            constant: true,
-            inputs: [{internalType: 'address',name: '_user',type: 'address'}],
-            name: 'balanceOf',
-            outputs: [{internalType: 'uint256',name: '',type: 'uint256'}],
-            payable: false,stateMutability: 'view',type: 'function'
-        },
-        {
-          constant: false,
-            inputs: [],
-            name: 'collect',
-            outputs: [{internalType: 'uint256',name: '',type: 'uint256'}],
-            payable: false,stateMutability: 'nonpayable',type: 'function'
-        },
-        {
-            constant: false,
-            inputs: [{internalType: 'uint256',name: '_tokens',type: 'uint256'}],
-            name: 'freeze',
-            outputs: [],
-            payable: false,
-            stateMutability: 'nonpayable',
-            type: 'function'
-        },
-        {
-            constant: false,
-            inputs: [
-                {internalType: 'address',name: '_to',type: 'address'},
-                {internalType: 'uint256',name: '_tokens',type: 'uint256'}
-            ],
-            name: 'transfer',
-            outputs: [{internalType: 'bool',name: '',type: 'bool'}],
-            payable: false,stateMutability: 'nonpayable',type: 'function'
-        },
-        {
-            constant: false,
-            inputs: [
-                {internalType: 'address',name: '_to',type: 'address'},
-                {internalType: 'uint256',name: '_tokens',type: 'uint256'},
-                {internalType: 'bytes',name: '_data',type: 'bytes'}
-            ],
-            name: 'transferAndCall',
-            outputs: [{internalType: 'bool',name: '',type: 'bool'}],
-            payable: false,stateMutability: 'nonpayable',type: 'function'
-        },
-        {
-            constant: false,
-            inputs: [{internalType: 'uint256',name: '_tokens',type: 'uint256'}],
-            name: 'unfreeze',
-            outputs: [],
-            payable: false,
-            stateMutability: 'nonpayable',
-            type: 'function'
-        }
-    ];
+    var abi = [{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"dividendsOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_receivers","type":"address[]"},{"name":"_amounts","type":"uint256[]"}],"name":"bulkTransfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tokens","type":"uint256"}],"name":"unstake","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"isWhitelisted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokens","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tokens","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"allInfoFor","outputs":[{"name":"totalTokenSupply","type":"uint256"},{"name":"totalTokensStaked","type":"uint256"},{"name":"userBalance","type":"uint256"},{"name":"userStaked","type":"uint256"},{"name":"userDividends","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalStaked","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_tokens","type":"uint256"}],"name":"distribute","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_tokens","type":"uint256"}],"name":"stake","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"stakedOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"collect","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_user","type":"address"},{"name":"_status","type":"bool"}],"name":"whitelist","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"user","type":"address"},{"indexed":false,"name":"status","type":"bool"}],"name":"Whitelist","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Stake","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Unstake","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Collect","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Burn","type":"event"}];
     
-    var address = 'TB2wzW2owPsahfwu4YwhxJXU2Pc9CrhfLa';
+    var address = 'TJ83VbkoGymcC358FBCQfhydE76bhfAr8r'; // Mainnet
+    // var address = 'TB2wzW2owPsahfwu4YwhxJXU2Pc9CrhfLa'; // Shasta Testnet
     var FLUX = await tronWeb.contract().at(address);
 
     function init() {
         setInterval(()=>{update()},3000)
-        $('#freezeToggle .nav-link').click(function () {
-            $('#freezeToggle .nav-link').removeClass('active');
+        $('#stakeToggle .nav-link').click(function () {
+            $('#stakeToggle .nav-link').removeClass('active');
             $(this).addClass('active');
             var toggle = $(this).attr('toggle');
-            $('.freeze, .unfreeze').hide();
+            $('.stake, .unstake').hide();
             $('.' + toggle).show();
         });
         $('#transfer').click(async function (event) {
@@ -87,19 +21,19 @@ async function run() {
             var to = $('#transferReceiver').val();
             if (amount > 0 && to.length == 34) {try {await FLUX.transfer(to, value).send();} catch (err) {console.error(err)}}
         });
-        $('#freeze').click(async function (event) {
+        $('#stake').click(async function (event) {
             event.preventDefault();
-            var amount = parseInt($('#freezeAmount').val());
+            var amount = parseInt($('#stakeAmount').val());
             let value = amount.toString() + "000000";
             console.log(value)
-            if (amount > 0) {try {await FLUX.freeze(value).send();} catch (err) {console.error(err)}}
+            if (amount > 0) {try {await FLUX.stake(value).send();} catch (err) {console.error(err)}}
         });
-        $('#unfreeze').click(async function (event) {
+        $('#unstake').click(async function (event) {
             event.preventDefault();
-            var amount = parseInt($('#unfreezeAmount').val());
+            var amount = parseInt($('#unstakeAmount').val());
             let value = amount.toString() + "000000";
             console.log(value)
-            if (amount > 0) {try {await FLUX.unfreeze(value).send();} catch (err) { console.error(err)}}
+            if (amount > 0) {try {await FLUX.unstake(value).send();} catch (err) { console.error(err)}}
         });
         $('#withdraw').click(function () {FLUX.collect().send(function (error, hash) {if (!error) {console.log(hash);} else {console.log(error);}});});
         setTimeout(update, 500);
@@ -112,9 +46,9 @@ async function run() {
           if (!error) {
               console.log(info);
               $('#totalSupply').text(formatNumber(parseFloat(info.totalTokenSupply / 1e6), 5));
-              $('#totalFrozen').text(formatNumber(parseFloat(info.totalTokensFrozen / 1e6), 5));
+              $('#totalStaked').text(formatNumber(parseFloat(info.totalTokensStaked / 1e6), 5));
               $('#myTokens').text(formatNumber(parseFloat(info.userBalance / 1e6), 5));
-              $('#myFrozen').text(formatNumber(parseFloat(info.userFrozen / 1e6), 5));
+              $('#myStaked').text(formatNumber(parseFloat(info.userStaked / 1e6), 5));
               $('#myDividends').text(formatNumber(parseFloat(info.userDividends / 1e6), 5));
               $('#withdrawAmount').text(formatNumber(parseFloat(info.userDividends / 1e6), 5));
           } else {console.log(error);}});
